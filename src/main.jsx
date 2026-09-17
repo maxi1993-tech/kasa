a74e57d10359db9cd0d186305129d5bd0615c9c4
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-17 20:08:15 +0200

    refactor: rename styles directory to sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import './styles/main.scss'
+import './sass/main.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
