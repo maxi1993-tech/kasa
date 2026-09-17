c656955dcdb652ecc72de6ff13552d339606e71f
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-17 16:34:57 +0200

    feat: add all routes with placeholder pages

---
+++ src/pages/Housing.jsx
@@ -0,0 +1,7 @@
+function Housing() {
+
+    return (
+        <h1>Logement</h1>
+    )
+}
+export default Housing
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router';
 import Home from '../pages/Home';
+import Housing from '../pages/Housing';
+import About from '../pages/About';
+import NotFound from '../pages/NotFound';
 
 function AppRouter() {
 
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/logement/:id" element={<Housing />} />
+            <Route path="/a-propos" element={<About />} />
+            <Route path="/erreur" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
