ac7a5ef1a42a32ea6207337a4ac4a378fb95ba9b
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-17 15:30:04 +0200

    feat: set up routing with home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import AppRouter from './routes/AppRouter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <AppRouter />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router';
+import Home from '../pages/Home';
+
+function AppRouter() {
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    )
+}
+
+export default AppRouter
