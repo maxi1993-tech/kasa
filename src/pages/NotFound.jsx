10007a408aa25a7ae2498eab66062720a6d78b73
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-22 17:40:20 +0200

    feat: build not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router"
+
+
 function NotFound() {
 
     return (
-        <h1>404</h1>
+        <section className="not-found">
+            <h1 className="not-found__title">404</h1>
+            <p className="not-found__text">Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="not-found__link" to="/">Retourner sur la page d'accueil</Link>
+        </section>
     )
 }
 export default NotFound
