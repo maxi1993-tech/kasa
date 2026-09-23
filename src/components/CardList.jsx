c6b954c8716ce36bc7cd113ae06a271a69bfefdb
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-23 13:53:51 +0200

    refactor: rename cardList class to card-list

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 function CardList({ housings }) {
 
     return (
-        <ul className="cardList">
+        <ul className="card-list">
             {housings.map((housing) => (
                 <Card key={housing.id} id={housing.id} title={housing.title} cover={housing.cover} />
             ))}
