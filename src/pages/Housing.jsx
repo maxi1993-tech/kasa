6efb2c38359ad7f8b4303bebf33b40600f0b4a86
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-24 09:32:50 +0200

    feat: display housing title from URL id

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,7 +1,20 @@
+import { useParams } from "react-router"
+import useFetchHousings from '../hooks/useFetchHousings'
+
 function Housing() {
 
+    const { id } = useParams()
+    const housings = useFetchHousings()
+
+    const housing = housings.find((item) => item.id === id)
+    console.log(housings)
+
+    if (!housing) {
+        return null
+    }
+
     return (
-        <h1>Logement</h1>
+        <h1>{housing.title}</h1>
     )
 }
 export default Housing
