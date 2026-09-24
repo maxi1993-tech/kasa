f66c5db771348d13a6bc25c7904aa05c8bb391d8
maxi1993-tech <maxi1993.cm@gmail.com>
2026-09-24 14:49:36 +0200

    feat: display housing details on housing page

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router"
-import useFetchHousings from '../hooks/useFetchHousings'
-import Slideshow from '../components/Slideshow'
+import useFetchHousings from "../hooks/useFetchHousings"
+import Slideshow from "../components/Slideshow"
+import starActive from "../assets/star-active.svg"
+import starInactive from "../assets/star-inactive.svg"
+import Collapse from "../components/Collapse"
 
 
 function Housing() {
@@ -10,6 +13,8 @@ function Housing() {
 
     const housing = housings.find((item) => item.id === id)
 
+    const stars = [1, 2, 3, 4, 5]
+
     if (!housing) {
         return null
     }
@@ -17,8 +22,37 @@ function Housing() {
     return (
         <>
             <Slideshow pictures={housing.pictures} />
-            <h1>{housing.title}</h1>
+            <section className="housing">
+                <div className="housing__heading">
+                    <h1 className="housing__title">{housing.title}</h1>
+                    <p className="housing__location">{housing.location}</p>
+                    <ul className="housing__tags">
+                        {housing.tags.map((tag) => (
+                            <li key={tag}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="housing__meta">
+                    <div className="housing__ratings">
+                        {stars.map((star) => (
+                            <img className="housing__star" key={star} src={star <= housing.rating ? starActive : starInactive} alt="" />
+                        ))}
+                        <span className="sr-only">Note de {housing.rating} sur 5</span>
+                    </div>
+                    <figure className="housing__host">
+                        <figcaption className="housing__hostname">{housing.host.name}</figcaption>
+                        <img className="housing__avatar" src={housing.host.picture} alt="" />
+                    </figure>
+                </div>
+                <Collapse title="Description" description={housing.description} />
+                <Collapse title="Équipements" description={<ul>
+                    {housing.equipments.map((equipment) => (
+                        <li key={equipment}>{equipment}</li>
+                    ))}
+                </ul>} />
+            </section>
         </>
     )
 }
 export default Housing
+
